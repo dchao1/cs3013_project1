52b91da37203bd66bdf7cbb656e67419162dd0e5
dchao1 <dchao@wpi.edu>
2019-01-16 12:45:05 -0500

    add fork, exec, and times

--- mc0.c
+++ mc0.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 //#include <mc0.h>
 
 int main() {
@@ -9,14 +11,28 @@ int main() {
 	printf("   1. last    : Prints out the result of the last command\n");
 	printf("   2. ls      : Prints out the result of a listing on a user-specified path\n");
 	
-	char* name = (char*) malloc(sizeof(char*));
+	char* comm = (char*) malloc(sizeof(char*));
 	printf("Option?: ");
-	scanf("%c", &name);
-	if (&name == "0") {
-		printf("\nwhat\n");
-	}
-	//printf("%s", name);
+	scanf("%c", comm);
+	printf(comm);
 	
+	clock_t timey = clock();
+	double time =((double)timey)/CLOCKS_PER_SEC;
+	printf("%f seconds", time);
+	
+	if (fork() ==0) {
+		printf("i am child\n");
+		execl("/bin/sh", "/bin/sh", "-c", "whoami", 0);
+		printf("done being child\n");
+	}
+	else {
+		wait();
+		printf("i am parent\n");
+		printf("child should be done\n");
+	}
+	
+	//printf("%s", name);
+	printf("safe");
 
 	return(0);
 }
