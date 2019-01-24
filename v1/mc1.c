bfd9b0658ce8ef14813ca52e7d1f0ae9c4d01af5
dchao1 <dchao@wpi.edu>
2019-01-23 23:16:49 -0500

    got the array of custom user commands working, can read commands and display correctly at the menu

--- v1/mc1.c
+++ v1/mc1.c
@@ -9,16 +9,30 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 #include "mc1.h"
+#include <inttypes.h>
+#include <errno.h>
 
 int midDayComm();
 
-
+struct newCommands {
+		int numElements;
+		//create array of ten char* with 30 chars each
+		char input[10][30];
+};
 
 int main() {
+
+
+	struct newCommands *newCs = malloc(1*sizeof(struct newCommands));
+
 	
+	//======
+	//activate our commander
 	//keep running until user forcefully exits
 	while (1) {
-		midDayComm();
+		printf("entering while loop\n");
+		midDayComm(newCs);
+		//midDayComm(newCommands);
 	}
 
 	
@@ -28,24 +42,43 @@ int main() {
 
 }
 
-int midDayComm() {
+//int midDayComm(char* newCommands) {
+int midDayComm(struct newCommands* newCs) {	
 	
+	//endptr is a thing needed to make strtoi work in the accessing the array of comm
+	char* endptr;
+	
+	
+	strcpy( (*newCs).input[0],  "ls" );
+	strcpy( (*newCs).input[1], "pwd" );
+	(*newCs).numElements = 2;
+	printf("%s, %s", (*newCs).input[0], (*newCs).input[1]);
+
+	int numElements = (*newCs).numElements;
+	
+
+
+
 	// Introduce our program
 	printf("===== Mid-Day Commander, v0 =====\n");
 	printf("G'day, Commander! What command would you like to run?\n");
 	printf("   0. whoami  : Prints out the result of the whoami command\n");
 	printf("   1. last    : Prints out the result of the last command\n");
 	printf("   2. ls      : Prints out the result of a listing on a user-specified path\n");
+	//here, we interrupt to print out our custom commands, if any
+	
+	for (int i = 0; i < numElements; i++){
+		printf("   %d. %s : User added command\n", i+3, (*newCs).input[i]);
+	}
+	
+	//continue with the scheduled commands
+	printf("   a. add command : Adds a new command to the menu.\n");
 	
 	//we create a character to hold what the user gives us
-
 	char* comm = (char*)malloc(128*sizeof(char*));
 	printf("Option?: ");
 	fgets(comm, 128, stdin);
-	//printf(comm);
-	comm = strtok(comm, "\n");
-	//printf(comm2);
-	//scanf("%c", comm); //actually ask the user for the code
+	comm = strtok(comm, "\n");//actually ask the user for the code
 	
 	//start the timer
 	clock_t timey = clock();
@@ -97,9 +130,49 @@ int midDayComm() {
 			// execute the command
 			execl("/bin/sh", "/bin/sh", "-c", command, (char *)0);
 		}
-		else {
-			printf ("this is the default\n");
+		//a for add command
+		else if (0 == strcmp(comm, "a")) {
+			printf("\n\n");
+			printf("-- Add a command --\n");
+			
+			// ask for the command input and put it into a 
+			/*
+			char* args = (char*)malloc(128*sizeof(char*));
+			printf("Arguments?: ");
+			fgets(args, 128, stdin);
+			args = strtok(args, "\n");
+			strcat(command, args);
+			strcat(command, " ");*/
+			
+			
 		}
+		//else, if it is none of the original commands, cycle through our array to see if we have a match
+
+		//might need to check for error above
+		//we have the entered value, presumably one of our new commands
+		else if (strtoimax(comm, &endptr, 0) > 2) {
+			//get the inputVal
+			int inputVal = strtoimax(comm, &endptr, 0);
+			for (int i = 0; i < numElements; i++) {
+				//now check the two
+				if (i+3 == inputVal) {
+					//store the userCommand locally, for convenience
+					char* userCommand = (*newCs).input[i];
+					
+					printf("\n\n");
+					printf("-- Command: %s --\n", userCommand);
+					
+					//run the user command
+					execl("/bin/sh", "/bin/sh", "-c", userCommand, (char *)0);
+					
+					//we have our command, stop checking
+					break;
+				}
+			}
+		}
+		//trash
+		printf ("this is the default\n");
+		
 		
 		return 1;
 	}
@@ -129,6 +202,8 @@ int midDayComm() {
 		return 1;
 
 	}
+	
+	//done
 	return 1;
 }
 
